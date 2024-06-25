12aa11f17e7298828188bd515df9fd23c139bd16
Yotam Greenstein <yotamgr314@gmail.com>
2024-06-25 21:14:21 +0300

    Implemented google maps API
    used google maps API key to enable a map via JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-function initMap()
-{
+function initMap() {
     const options = {
         zoom: 8,
-        center:{lat: 32.28967359860627, lng: -34.850024746828495}
-
-    }
-        let map = new google.maps.Map(document.getElementById('map'),options);
+        center: {lat: 32.28971894713777, lng: 34.850046204500295} // Sdeort HaAgamim 11, Netanya coordinates
+    };
+    let map = new google.maps.Map(document.getElementById('map'), options);
 }
 
+
 initMap();
+
+32.28971894713777, 34.850046204500295
